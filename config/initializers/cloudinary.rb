ef5d62523b872cd5462855c04a4d5ffe322d6595
Rinat <rinenik3105@gmail.com>
2023-05-12 00:05:48 +0300

    Setup cloud storage

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'dwihus8yq'
+  config.api_key = '875356529379332'
+  config.api_secret = 'v1O9dg_pZlFF128X45ppZcpz4PA'
+  config.secure = true
+end
